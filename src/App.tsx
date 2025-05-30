a0d9a353ee4abc1432f1d412062bb6f80cfa1bde
SebastianFurnier <sfurnier@hotmail.com>
2025-05-30 15:53:38 -0300

    Reorganizo secciones

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-import Home from './components/home'
+import Home from './pages/home'
 import Navbar from './components/Navbar'
-import Proyects from './components/proyects'
-import AboutMe from './components/AboutMe'
+import Proyects from './sections/proyects'
+import AboutMe from './sections/AboutMe'
 import ScrollDownHint from './components/ScrollDownHint'
 import Sep from './components/sep'
-import Contact from './components/Contact'
-import Technologies from './components/technologies'
+import Contact from './sections/Contact'
+import Technologies from './sections/technologies'
 
 function App() {
 
